773a9347ff0af2b12169cee8204471a9850a8ff3
Juan <49161149+Juandials@users.noreply.github.com>
2019-09-10 17:45:19 -0500

    Add files via upload

--- src/vehiculo/pkg1/usuario.java
+++ src/vehiculo/pkg1/usuario.java
@@ -1,24 +1,31 @@
-
 package vehiculo.pkg1;
 
 import javax.swing.JOptionPane;
 import java.util.Scanner;
-public class usuario {
-    String contra;
-    Scanner sc=new Scanner(System.in);
-   
-        String name, contrasena;
- public void clave(){
-contrasena="malia1804";
-}
- 
-     public void setname(){
-    name=JOptionPane.showInputDialog("ingrese nombre");
- 
-  }
-      public boolean ValidacionContraseña(String contra){
-         
-          return (contrasena.equals(contra));
 
-      }
+public class usuario {
+
+    String contra;
+    Scanner sc = new Scanner(System.in);
+
+    String name, contrasena;
+
+    public void clave() {
+        contrasena = "malia1804";
+    }
+
+    public void setname() {
+        name = JOptionPane.showInputDialog("ingrese nombre");
+
+    }
+
+    public boolean ValidacionContraseña(String contra) {
+        if (contra.equals(this.contrasena)) {
+
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
